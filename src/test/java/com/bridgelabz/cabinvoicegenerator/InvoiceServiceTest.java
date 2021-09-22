204b22889349504b5346b29b7627357248a7595f
varshini <dreamvarshini@gmail.com>
2021-09-22 10:58:50 +0530

    ADDED: testcase to check function returns correct fair of multiple rides

--- src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
@@ -26,8 +26,18 @@ public class InvoiceServiceTest
 		int time=1;
 		double fare=invoiceGenerator.calculateFare(distance,time);
 		Assert.assertEquals(5, fare,0.0);
-
-
 	}
+	
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalFare()
+    {
+    	InvoiceGenerator invoiceGenerator= new InvoiceGenerator();
+    	Ride[] rides= {new Ride(2.0,5),
+    				 new Ride(0.1,1)};
+    	
+    	double fare=invoiceGenerator.calculateFare(rides);
+		Assert.assertEquals(30, fare,0.0);
+
+    }
 
 }
