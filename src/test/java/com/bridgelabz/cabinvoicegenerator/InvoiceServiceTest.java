c99e520055404460ea76e200911fc1e44e55eb39
varshini <dreamvarshini@gmail.com>
2021-09-22 10:52:57 +0530

    ADDED: test case to check function returning minimum fair for the ride

--- src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
@@ -8,15 +8,26 @@ import org.junit.Test;
 public class InvoiceServiceTest 
 {
 
-	 @Test
-	    public void givenDistanceAndTimeShouldReturnTheTotalFare() 
-	    {
-	    	InvoiceGenerator invoiceGenerator= new InvoiceGenerator();
-	    	double distance=2.0;
-	    	int time=5;
-	    	double fare=invoiceGenerator.calculateFare(distance,time);
-	    	Assert.assertEquals(25, fare,0.0);
-	    }
-	
+	@Test
+	public void givenDistanceAndTimeShouldReturnTheTotalFare() 
+	{
+		InvoiceGenerator invoiceGenerator= new InvoiceGenerator();
+		double distance=2.0;
+		int time=5;
+		double fare=invoiceGenerator.calculateFare(distance,time);
+		Assert.assertEquals(25, fare,0.0);
+	}
+
+	@Test
+	public void givenLessDistanceOrTime_ShouldReturnMinimumFare()
+	{
+		InvoiceGenerator invoiceGenerator= new InvoiceGenerator();
+		double distance=0.1;
+		int time=1;
+		double fare=invoiceGenerator.calculateFare(distance,time);
+		Assert.assertEquals(5, fare,0.0);
+
+
+	}
 
 }
