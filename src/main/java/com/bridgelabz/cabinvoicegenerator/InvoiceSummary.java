403b463462c492c89193999c36e85f8f0f08b252
varshini <dreamvarshini@gmail.com>
2021-09-22 11:09:43 +0530

    ADDED: overriding equals method to match object properties

--- src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceSummary.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceSummary.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.cabinvoicegenerator;
 
+import java.util.Objects;
+
 public class InvoiceSummary 
 {
 	public int numberOfRides;
@@ -14,4 +16,19 @@ public class InvoiceSummary
 		this.averageFair=this.totalFare/this.numberOfRides;
 	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		InvoiceSummary other = (InvoiceSummary) obj;
+		return Double.doubleToLongBits(averageFair) == Double.doubleToLongBits(other.averageFair)
+				&& numberOfRides == other.numberOfRides
+				&& Double.doubleToLongBits(totalFare) == Double.doubleToLongBits(other.totalFare);
+	}
+
+	
 }
