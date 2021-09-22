f7c21369ada8ee1ef0326ad491ed6648d164b774
varshini <dreamvarshini@gmail.com>
2021-09-22 10:53:26 +0530

    MODIFIED: calculate fair function to return minimum fair of the ride

--- src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
@@ -4,10 +4,18 @@ public class InvoiceGenerator
 {
 	private static final double MINIMUM_COST_PER_KILOMETER = 10;
 	private static final int COST_PER_TIME = 1;
+	private static final double MINIMUM_FARE = 5;
+
 
 	public double calculateFare(double distance, int time) 
 	{
-		return distance*MINIMUM_COST_PER_KILOMETER+time*COST_PER_TIME;
+		double totalFare= distance*MINIMUM_COST_PER_KILOMETER+time*COST_PER_TIME;
+		if(totalFare<MINIMUM_FARE)
+		{
+			return MINIMUM_FARE;
+		}
+		
+			return totalFare;
 	}
 
 }
