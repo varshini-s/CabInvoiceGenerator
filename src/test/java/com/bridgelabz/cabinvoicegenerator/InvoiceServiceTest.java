72d21c02277a7ab6eac3797a36e7b7f46e53ac22
varshini <dreamvarshini@gmail.com>
2021-09-22 11:15:47 +0530

    REFACTORED: using before annotation for test to reduce code repetetion

--- src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
@@ -3,15 +3,22 @@ package com.bridgelabz.cabinvoicegenerator;
 import static org.junit.Assert.*;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceServiceTest 
 {
+	InvoiceGenerator invoiceGenerator;
+	@Before
+	public void initialSetup()
+	{
+		invoiceGenerator= new InvoiceGenerator();
+		
+	}
 
 	@Test
 	public void givenDistanceAndTimeShouldReturnTheTotalFare() 
 	{
-		InvoiceGenerator invoiceGenerator= new InvoiceGenerator();
 		double distance=2.0;
 		int time=5;
 		double fare=invoiceGenerator.calculateFare(distance,time);
@@ -21,7 +28,6 @@ public class InvoiceServiceTest
 	@Test
 	public void givenLessDistanceOrTime_ShouldReturnMinimumFare()
 	{
-		InvoiceGenerator invoiceGenerator= new InvoiceGenerator();
 		double distance=0.1;
 		int time=1;
 		double fare=invoiceGenerator.calculateFare(distance,time);
@@ -31,7 +37,6 @@ public class InvoiceServiceTest
     @Test
     public void givenMultipleRides_ShouldReturnTotalFare()
     {
-    	InvoiceGenerator invoiceGenerator= new InvoiceGenerator();
     	Ride[] rides= {new Ride(2.0,5),
     				 new Ride(0.1,1)};
     	
