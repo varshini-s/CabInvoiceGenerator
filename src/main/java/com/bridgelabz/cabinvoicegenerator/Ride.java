bcd76206e04a18844890a6f1001c0fce700ded55
varshini <dreamvarshini@gmail.com>
2021-09-22 13:39:14 +0530

    REFACTORED: defined getters and setters for private members of class

--- src/main/java/com/bridgelabz/cabinvoicegenerator/Ride.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/Ride.java
@@ -7,22 +7,59 @@ public class Ride
 		NORMAL_RIDE,PREMIUM_RIDE;
 	}
 
-	public double distance;
-	public double time;
-	public RideType rideType;
+	private double distance;
+	private double time;
+	private RideType rideType;
 
 	public Ride(double distance,int time)
 	{
 		this.distance=distance;
 		this.time=time;
 	}
-	
-	
+
+
 	public Ride(double distance,double time,RideType rideType)
 	{
 		this.distance=distance;
 		this.time=time;
 		this.rideType=rideType;
 	}
+
+
+	public double getDistance()
+	{
+		return distance;
+	}
+
+
+	public void setDistance(double distance)
+	{
+		this.distance = distance;
+	}
+
+
+	public double getTime() 
+	{
+		return time;
+	}
+
+
+	public void setTime(double time)
+	{
+		this.time = time;
+	}
+
+
+	public RideType getRideType() 
+	{
+		return rideType;
+	}
+
+
+	public void setRideType(RideType rideType)
+	{
+		this.rideType = rideType;
+	}
+
 
 }
