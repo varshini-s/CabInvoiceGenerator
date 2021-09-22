d759f1c3a896ea23d3c57214835d0f122817902c
varshini <dreamvarshini@gmail.com>
2021-09-22 10:51:10 +0530

    ADDED: defining constants to calculate ride fair

--- src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
@@ -2,7 +2,8 @@ package com.bridgelabz.cabinvoicegenerator;
 
 public class InvoiceGenerator 
 {
-	
+	private static final double MINIMUM_COST_PER_KILOMETER = 10;
+	private static final int COST_PER_TIME = 1;
 
 	public double calculateFare(double distance, int time) 
 	{
