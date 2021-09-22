bc127ffd388e9c5b9f0f8f94c104046cb655e10d
varshini <dreamvarshini@gmail.com>
2021-09-22 11:03:09 +0530

    MODIFIED: defining variable valus for constructor of ride class

--- src/main/java/com/bridgelabz/cabinvoicegenerator/Ride.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/Ride.java
@@ -1,11 +1,15 @@
 package com.bridgelabz.cabinvoicegenerator;
 
-public class Ride {
+public class Ride 
+{
+
+	public double distance;
+	public int time;
 
 	public Ride(double distance,int time)
 	{
-
-		
+		this.distance=distance;
+		this.time=time;
 	}
 
 }
