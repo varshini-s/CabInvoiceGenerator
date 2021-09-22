4f5a87408b85418e341b0328d704bcba66bb23d6
varshini <dreamvarshini@gmail.com>
2021-09-22 10:41:14 +0530

    ADDED: main class to print welcome messsage

---
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGeneratorMain.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+public class CabInvoiceGeneratorMain 
+{
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to cab invoice generator");
+		
+	}
+
+}
