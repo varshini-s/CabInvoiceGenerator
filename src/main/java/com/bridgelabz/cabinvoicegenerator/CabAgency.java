0d207ce5dd652ebb55daf2ea839dfa3297be49d3
varshini <dreamvarshini@gmail.com>
2021-09-22 13:28:23 +0530

    ADDED: defined constructor to add given cost of normal and premium rides

--- src/main/java/com/bridgelabz/cabinvoicegenerator/CabAgency.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/CabAgency.java
@@ -15,5 +15,28 @@ public class CabAgency
 
 	CostType costType;
 	
+	Map<Ride.RideType,Map<CostType,Double>> rideDetail= new HashMap<Ride.RideType, Map<CostType,Double>>();
+	
+	public CabAgency(double normalRideCostPerKilometer,double normalRideCostPerMinute,double normalRideMinimumFare,
+					 double preimumRideCostPerKilometer,double premiumRideCostPerMinute,double premiumRideMinimumFare) 
+	{
+		
+		Map<CostType,Double> normalRideCostDetails=new HashMap<CabAgency.CostType, Double>();
+		normalRideCostDetails.put(costType.COST_PER_KILOMETER, normalRideCostPerKilometer);
+		normalRideCostDetails.put(costType.COST_PER_MINUTE, normalRideCostPerMinute);
+		normalRideCostDetails.put(costType.MINIMUM_FARE, normalRideMinimumFare);
+
+		
+		Map<CostType, Double> premiumRideCostDetails= new HashMap<CabAgency.CostType, Double>();
+		premiumRideCostDetails.put(costType.COST_PER_KILOMETER, preimumRideCostPerKilometer);
+		premiumRideCostDetails.put(costType.COST_PER_MINUTE, premiumRideCostPerMinute);
+		premiumRideCostDetails.put(costType.MINIMUM_FARE, premiumRideMinimumFare);
+
+		
+		this.rideDetail.put(Ride.RideType.NORMAL_RIDE, normalRideCostDetails);
+		this.rideDetail.put(Ride.RideType.PREMIUM_RIDE, premiumRideCostDetails);
+
+	}
+
 	
 }
