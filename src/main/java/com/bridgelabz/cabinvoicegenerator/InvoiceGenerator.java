5afa7dbb526f2df49f9a167eb06bb4a777e09479
varshini <dreamvarshini@gmail.com>
2021-09-22 10:50:44 +0530

    MODIFIED: function body to calculate fair to pass the given test case

--- src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceGenerator.java
@@ -2,10 +2,11 @@ package com.bridgelabz.cabinvoicegenerator;
 
 public class InvoiceGenerator 
 {
+	
 
 	public double calculateFare(double distance, int time) 
 	{
-		return 0;
+		return distance*MINIMUM_COST_PER_KILOMETER+time*COST_PER_TIME;
 	}
 
 }
