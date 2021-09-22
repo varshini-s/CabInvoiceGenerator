711330f648b9f8e0e7d332b6f8485c8ccb4e2322
varshini <dreamvarshini@gmail.com>
2021-09-22 13:27:21 +0530

    ADDED: cab agency class wtih cost type enum defined

---
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/CabAgency.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+import java.util.HashMap;
+import java.util.Map;import org.checkerframework.checker.units.qual.C;
+
+public class CabAgency 
+{
+
+	
+	
+	enum CostType
+	{
+		COST_PER_KILOMETER,COST_PER_MINUTE,MINIMUM_FARE
+	}
+
+	CostType costType;
+	
+	
+}
