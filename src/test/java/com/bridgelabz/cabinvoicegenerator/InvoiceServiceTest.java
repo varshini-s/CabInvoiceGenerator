3f2129aba6193edf019d4d9be46f73119b3a2de0
varshini <dreamvarshini@gmail.com>
2021-09-22 13:24:55 +0530

    ADDED: test case to check total fare for both ride types

--- src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceServiceTest.java
@@ -6,6 +6,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.bridgelabz.cabinvoicegenerator.Ride.RideType;
+
 public class InvoiceServiceTest 
 {
 	InvoiceGenerator invoiceGenerator;
@@ -59,6 +61,25 @@ public class InvoiceServiceTest
     	InvoiceSummary summary =invoiceGenerator.calculateFare(userID,rideRepository);
     	InvoiceSummary expectedInvoiceSummary=new InvoiceSummary(2,30);
     	Assert.assertEquals(summary,expectedInvoiceSummary);
+    	
+    }
+    
+    @Test
+    public void givenUserID_ShouldReturnTotalFareOfAllTheRidesForBothRideTypes()
+    {
+    	
+    	CabAgency cabAgency = new CabAgency(10, 1,5,
+    							        	15, 2,20);
+    	RideRepository rideRepository=new RideRepository();
+    	Ride[] rides={new Ride(2.0,5,RideType.NORMAL_RIDE),
+    				  new Ride(2.0,5,RideType.PREMIUM_RIDE)};
+    	String userID="user1";
+    	rideRepository.addRideList(userID,rides);
+    	
+    	InvoiceSummary summary =invoiceGenerator.calculateFare(userID,rideRepository,cabAgency);
+    	InvoiceSummary expectedInvoiceSummary=new InvoiceSummary(2,65);
+    	Assert.assertEquals(summary,expectedInvoiceSummary);
+    	
     	
     }
 
