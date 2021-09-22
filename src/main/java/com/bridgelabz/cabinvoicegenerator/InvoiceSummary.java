792328c8f6c7e7b9f3cf68393b81c8dd9a2dc156
varshini <dreamvarshini@gmail.com>
2021-09-22 11:08:53 +0530

    MODIFIED: defining variables and constructor for invoicesummary class

--- src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceSummary.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceSummary.java
@@ -2,9 +2,16 @@ package com.bridgelabz.cabinvoicegenerator;
 
 public class InvoiceSummary 
 {
+	public int numberOfRides;
+	public double totalFare;
+	public double averageFair;
 
 	public InvoiceSummary(int numberOfRides,double totalFare) 
 	{
+
+		this.numberOfRides=numberOfRides;
+		this.totalFare=totalFare;
+		this.averageFair=this.totalFare/this.numberOfRides;
 	}
 
 }
