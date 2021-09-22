18819c5deda65c8efcb43fa288e74ea7a89659bc
varshini <dreamvarshini@gmail.com>
2021-09-22 13:25:34 +0530

    MODIFIED: defining enum for ride type

--- src/main/java/com/bridgelabz/cabinvoicegenerator/Ride.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/Ride.java
@@ -2,14 +2,22 @@ package com.bridgelabz.cabinvoicegenerator;
 
 public class Ride 
 {
+	enum RideType
+	{
+		NORMAL_RIDE,PREMIUM_RIDE;
+	}
 
 	public double distance;
-	public int time;
+	public double time;
+	public RideType rideType;
 
 	public Ride(double distance,int time)
 	{
 		this.distance=distance;
 		this.time=time;
 	}
+	
+	
+
 
 }
